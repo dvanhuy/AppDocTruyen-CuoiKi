611ac702d5df570111265402255b6d96ff8c24ac
Duy <leducduydn.7122111@gmail.com>
2022-11-30 16:15:27 +0700

    add favor history

--- app/src/main/java/com/example/appdoctruyen_cuoiki/Fragment3.java
+++ app/src/main/java/com/example/appdoctruyen_cuoiki/Fragment3.java
@@ -1,18 +1,26 @@
 package com.example.appdoctruyen_cuoiki;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment3#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.appdoctruyen_cuoiki.LichSuTruyen.favBook.favBook;
+import com.example.appdoctruyen_cuoiki.LichSuTruyen.favBook.favBookAdapter;
+import com.example.appdoctruyen_cuoiki.LichSuTruyen.history.history;
+import com.example.appdoctruyen_cuoiki.LichSuTruyen.history.historyAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Fragment3 extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -24,17 +32,19 @@ public class Fragment3 extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Fragment3() {
-        // Required empty public constructor
-    }
+    private RecyclerView rcvFav, rcvHis;
+    private List<favBook> firstFavBook;
+    private List<history> firstHis;
 
+    public Fragment3() {
+    }
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment3.
+     * @return A new instance of fragment Fragment2.
      */
     // TODO: Rename and change types and number of parameters
     public static Fragment3 newInstance(String param1, String param2) {
@@ -49,6 +59,7 @@ public class Fragment3 extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -58,7 +69,35 @@ public class Fragment3 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_3, container, false);
+        View view = inflater.inflate(R.layout.fragment_3, container, false);
+
+        rcvFav = view.findViewById(R.id.rcv_favorBook);
+        favBookAdapter favBookAdapter = new favBookAdapter(getContext(),firstFavBook);
+        rcvFav.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rcvFav.setAdapter(favBookAdapter);
+
+        rcvHis = view.findViewById(R.id.rcv_HistoryBook);
+        historyAdapter historydapter = new historyAdapter(getContext(),firstHis);
+        //historyAdapter.setLayoutManager(new GridLayoutManager(getContext(), 3));
+        rcvHis.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rcvHis.setAdapter(historydapter);
+
+        initData();
+        return view;
+    }
+
+    public void initData() {
+
+        firstFavBook = new ArrayList<>();
+        firstFavBook.add(new favBook("Boruto",R.drawable.home_list_truyen1));
+        firstFavBook.add(new favBook("Tàn Chi Lệnh",R.drawable.home_list_truyen2));
+        firstFavBook.add(new favBook("Tình Yêu Trở Lại",R.drawable.home_list_truyen3));
+        firstFavBook.add(new favBook("Vương quốc bí ẩn",R.drawable.home_list_truyen4));
+
+        firstHis = new ArrayList<>();
+        firstHis.add(new history("Boruto",R.drawable.home_list_truyen1));
+        firstHis.add(new history("Tấm Cám",R.drawable.home_list_truyen5));
+        firstHis.add(new history("Truyện Kiều",R.drawable.home_list_truyen6));
+        firstHis.add(new history("Tình Yêu Trở Lại",R.drawable.home_list_truyen3));
     }
 }
---
+++ app/src/main/java/com/example/appdoctruyen_cuoiki/LichSuTruyen/favBook/favBookAdapter.java
@@ -0,0 +1,72 @@
+package com.example.appdoctruyen_cuoiki.LichSuTruyen.favBook;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.appdoctruyen_cuoiki.R;
+
+import java.util.List;
+
+public class favBookAdapter extends RecyclerView.Adapter<favBookAdapter.favBookViewHolder> {
+
+    Context mContext;
+    List<favBook> mListFavBook;
+
+    public favBookAdapter(Context mContext, List<favBook> mListFavBook) {
+        this.mContext = mContext;
+        this.mListFavBook = mListFavBook;
+    }
+
+    public void setData(List<favBook> list){
+        this.mListFavBook = list;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public favBookViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v;
+        v = LayoutInflater.from(mContext).inflate(R.layout.item_favbook,parent,false);
+        favBookViewHolder novelViewHolder = new favBookViewHolder(v);
+
+        return novelViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull favBookViewHolder holder, int position) {
+        favBook novel = mListFavBook.get(position);
+        if (novel == null){
+            return;
+        }
+
+        holder.hinhAnh.setImageResource(novel.getHinhAnh());
+        holder.ten.setText(novel.getTen());
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mListFavBook != null){
+            return mListFavBook.size();
+        }
+        return 0;
+    }
+
+
+    public class favBookViewHolder extends RecyclerView.ViewHolder{
+
+    TextView ten;
+    ImageView hinhAnh;
+    public favBookViewHolder(@NonNull View itemView) {
+        super(itemView);
+        ten = itemView.findViewById(R.id.img_favbook);
+        hinhAnh = itemView.findViewById(R.id.tv_favbook);
+    }
+}
+}
