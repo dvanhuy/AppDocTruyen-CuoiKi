dd4dc29f77a088a95b7d6eab2b6ca89b138b28bc
happynehihi <lephanhoangphuc2002@gmail.com>
2022-12-03 01:35:51 +0700

    login google

--- app/src/main/java/com/example/appdoctruyen_cuoiki/SignIn.java
+++ app/src/main/java/com/example/appdoctruyen_cuoiki/SignIn.java
@@ -9,16 +9,24 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SignIn extends AppCompatActivity {
-
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    TextView googleBtn;
     EditText txtEmail,txtPassWord;
     private FirebaseAuth mAuth;
     @Override
@@ -27,7 +35,6 @@ public class SignIn extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in);
 
         mAuth = FirebaseAuth.getInstance();
-
         TextView forgotpass = findViewById(R.id.forgotpass);
         forgotpass.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,7 +43,6 @@ public class SignIn extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
         TextView signupforgotpass = findViewById(R.id.signupforgotpass);
         signupforgotpass.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,33 +60,68 @@ public class SignIn extends AppCompatActivity {
                 Login();
             }
         });
+        googleBtn = findViewById(R.id.google_btn);
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this,gso);
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if(acct!=null){
+            navigateToSecondActivity();
+        }
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
     }
+
 
     private void Login() {
         String email, pass;
         email = txtEmail.getText().toString();
         pass = txtPassWord.getText().toString();
-
-        if(TextUtils.isEmpty(email)){
-            Toast.makeText(this,"Nhập Email !",Toast.LENGTH_SHORT).show();
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Nhập Email !", Toast.LENGTH_SHORT).show();
             return;
         }
-        if(TextUtils.isEmpty(pass)){
-            Toast.makeText(this,"Nhập mật khẩu !",Toast.LENGTH_SHORT).show();
+        if (TextUtils.isEmpty(pass)) {
+            Toast.makeText(this, "Nhập mật khẩu !", Toast.LENGTH_SHORT).show();
             return;
         }
-
         mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
-                    Toast.makeText(getApplicationContext(),"Đăng nhập thành công !", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Đăng nhập thành công !", Toast.LENGTH_SHORT).show();
                     setResult(RESULT_OK);
                     finish();
-                }
-                else {
-                    Toast.makeText(getApplicationContext(),"Đăng nhập thất bại !", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Đăng nhập thất bại !", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+    }
+
+    void signIn(){
+        Intent signInIntent = gsc.getSignInIntent();
+        startActivityForResult(signInIntent,1000);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1000){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                task.getResult(ApiException.class);
+                navigateToSecondActivity();
+            } catch (ApiException e) {
+                Toast.makeText(getApplicationContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    void navigateToSecondActivity(){
+        finish();
+        Intent intent = new Intent(SignIn.this,SignInGG.class);
+        startActivity(intent);
     }
 }
