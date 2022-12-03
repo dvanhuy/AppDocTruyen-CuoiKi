ebedf1d27ed16d96a945174681ed44bf3344f952
happynehihi <lephanhoangphuc2002@gmail.com>
2022-12-03 10:35:02 +0700

    link discord and fb

--- app/src/main/java/com/example/appdoctruyen_cuoiki/Cauhoithuonggap.java
+++ app/src/main/java/com/example/appdoctruyen_cuoiki/Cauhoithuonggap.java
@@ -2,14 +2,25 @@ package com.example.appdoctruyen_cuoiki;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Cauhoithuonggap extends AppCompatActivity {
-
+    ImageView backcauhoithuonggap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cauhoithuonggap);
+        backcauhoithuonggap=findViewById(R.id.backcauhoithuonggap);
+        backcauhoithuonggap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Cauhoithuonggap.this,Fragment4.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
