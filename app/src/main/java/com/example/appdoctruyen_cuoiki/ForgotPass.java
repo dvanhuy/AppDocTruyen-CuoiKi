3057a5d091c4ebd8e7062f59df441f3d6dea2e73
happynehihi <lephanhoangphuc2002@gmail.com>
2022-12-17 18:44:51 +0700

    FullApp

--- app/src/main/java/com/example/appdoctruyen_cuoiki/ForgotPass.java
+++ app/src/main/java/com/example/appdoctruyen_cuoiki/ForgotPass.java
@@ -4,17 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPass extends AppCompatActivity {
 
+    EditText txtemail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_pass);
         ImageView forgotpassback= findViewById(R.id.forgotpassback);
+        txtemail = findViewById(R.id.txtemail);
         forgotpassback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,8 +36,15 @@ public class ForgotPass extends AppCompatActivity {
         btnsubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ForgotPass.this,ResetPass.class);
-                startActivity(intent);
+                FirebaseAuth.getInstance().sendPasswordResetEmail(txtemail.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(ForgotPass.this, "Email sent.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
     }
